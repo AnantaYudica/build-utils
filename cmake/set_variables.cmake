1d2005961995b99a9d5cacfffe0696bbb4804650
Ananta Yudica <nta040689@gmail.com>
2020-04-07 17:07:23 +0700

    Added empty string validation of BASE_SOURCE_DIR, INCLUDE_DIR, TEST_DIR, TEST_UTILS_DIR and TEST_BUILD_UTILS_DIR

--- cmake/set_variables.cmake
+++ cmake/set_variables.cmake
@@ -4,7 +4,12 @@ function(set_variables)
         "BASE_SOURCE_DIR;INCLUDE_DIR;TEST_DIR;TEST_UTILS_DIR;TEST_BUILD_UTILS_DIR" "" ${ARGN}) 
 
     set(l_base_source_dir "")
-    get_filename_component(l_base_source_dir "${set_variables_BASE_SOURCE_DIR}" ABSOLUTE)
+
+    if("${set_variables_BASE_SOURCE_DIR}" STREQUAL "")
+        get_filename_component(l_base_source_dir "${CMAKE_SOURCE_DIR}" ABSOLUTE)
+    else()    
+        get_filename_component(l_base_source_dir "${set_variables_BASE_SOURCE_DIR}" ABSOLUTE)
+    endif()
 
     if (EXISTS "${l_base_source_dir}")
         set(BASE_SOURCE_DIR ${l_base_source_dir} CACHE PATH "project cmake directory" FORCE)
@@ -15,7 +20,12 @@ function(set_variables)
     endif()
     
     set(l_include_dir "")
-    get_filename_component(l_include_dir "${set_variables_INCLUDE_DIR}" ABSOLUTE)
+    
+    if("${set_variables_INCLUDE_DIR}" STREQUAL "")
+        get_filename_component(l_include_dir "${BASE_SOURCE_DIR}/../include" ABSOLUTE)
+    else()
+        get_filename_component(l_include_dir "${set_variables_INCLUDE_DIR}" ABSOLUTE)
+    endif()
 
     if (EXISTS "${l_include_dir}")
         set(INCLUDE_DIR ${l_include_dir} CACHE PATH "include directory" FORCE)
@@ -27,7 +37,12 @@ function(set_variables)
     endif()
     
     set(l_test_dir "")
-    get_filename_component(l_test_dir "${set_variables_TEST_DIR}" ABSOLUTE)
+
+    if ("${set_variables_TEST_DIR}" STREQUAL "")
+        get_filename_component(l_test_dir "${BASE_SOURCE_DIR}/test" ABSOLUTE)
+    else()
+        get_filename_component(l_test_dir "${set_variables_TEST_DIR}" ABSOLUTE)
+    endif()
 
     if (EXISTS "${l_test_dir}")
         set(TEST_DIR ${l_test_dir} CACHE PATH "test directory" FORCE)
@@ -39,7 +54,12 @@ function(set_variables)
     endif()
 
     set(l_test_utils_dir "")
-    get_filename_component(l_test_utils_dir "${set_variables_TEST_UTILS_DIR}" ABSOLUTE)
+
+    if ("${set_variables_TEST_UTILS_DIR}" STREQUAL "")
+        get_filename_component(l_test_utils_dir "${TEST_DIR}/utils" ABSOLUTE)
+    else()
+        get_filename_component(l_test_utils_dir "${set_variables_TEST_UTILS_DIR}" ABSOLUTE)
+    endif()
 
     if (EXISTS "${l_test_utils_dir}")
         set(TEST_UTILS_DIR ${l_test_utils_dir} CACHE PATH "test utils directory" FORCE)
@@ -51,8 +71,13 @@ function(set_variables)
     endif()
 
     set(l_test_build_utils_dir "")
-    get_filename_component(l_test_build_utils_dir "${set_variables_TEST_BUILD_UTILS_DIR}" ABSOLUTE)
 
+    if("${set_variables_TEST_BUILD_UTILS_DIR}" STREQUAL "")
+        get_filename_component(l_test_build_utils_dir "${TEST_DIR}/build_utils" ABSOLUTE)
+    else()
+        get_filename_component(l_test_build_utils_dir "${set_variables_TEST_BUILD_UTILS_DIR}" ABSOLUTE)
+    endif()
+    
     if (EXISTS "${l_test_build_utils_dir}")
         set(TEST_BUILD_UTILS_DIR ${l_test_build_utils_dir} CACHE PATH "test utils directory" FORCE)
     elseif(NOT EXISTS "${l_test_build_utils_dir}")

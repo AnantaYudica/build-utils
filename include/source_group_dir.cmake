fdbb6b37b4bd2c5945884f00af913f0ca9dbed9d
Ananta Yudica <nta040689@gmail.com>
2020-04-12 03:23:32 +0700

    Added function source_group_dir Added function source_group_dir_recv

---
+++ include/source_group_dir.cmake
@@ -0,0 +1,82 @@
+
+function(source_group_dir_recv prefix_name dir is_output_name is_output_path 
+    output_list_name output_list_path)
+
+    file(GLOB list_files "${dir}/*")
+
+    foreach(it ${list_files})
+        source_group_dir_filter(${it} ok)
+        get_filename_component(file_name ${it} NAME)
+        if (ok)
+            if(IS_DIRECTORY ${it}) 
+                source_group_dir_recv("${prefix_name}/${file_name}"
+                    "${dir}/${file_name}" ${is_output_name} ${is_output_path}
+                    ${output_list_name} ${output_list_path})
+            else()
+                source_group_dir_condition(${it} ok)
+                if (ok)
+                    if(NOT DEFINED CMAKE_SCRIPT_MODE_FILE)
+                        source_group(${prefix_name} FILES ${it})
+                    endif()
+
+                    if (is_output_name)
+                        set(tmp "${${output_list_name}};${prefix_name}")
+                        set(${output_list_name} ${tmp} PARENT_SCOPE)
+                    endif()
+                    if (is_output_path)
+                        set(tmp "${${output_list_path}};${it}")
+                        set(${output_list_path} ${tmp} PARENT_SCOPE)
+                    endif()
+                endif()
+            endif()
+        endif()
+    endforeach()
+
+endfunction(source_group_dir_recv)
+
+function(source_group_dir prefix_name dir)
+    cmake_parse_arguments(source_group_dir "" 
+        "FILTER;CONDITION;OUTPUT_LIST_NAME;OUTPUT_LIST_PATH;INCLUDE_DIR" "" ${ARGN}) 
+    
+    set(base_dir "${BUILD_UTILS_INCLUDE_DIR}")
+    if (NOT "${source_group_dir_INCLUDE_DIR}" STREQUAL "")
+        set(base_dir "${source_group_dir_INCLUDE_DIR}")
+    endif()
+
+    if("${source_group_dir_FILTER}" STREQUAL "" OR (NOT EXISTS "${source_group_dir_FILTER}"))
+        include(${base_dir}/source_group_dir/filter.cmake)
+    else()
+        include(${source_group_dir_FILTER})
+    endif()
+
+    if("${source_group_dir_CONDITION}" STREQUAL "" OR (NOT EXISTS "${source_group_dir_CONDITION}"))
+        include(${base_dir}/source_group_dir/condition.cmake)
+    else()
+        include(${source_group_dir_CONDITION})
+    endif()
+    
+    set(enable_output_name FALSE)
+    if(NOT "${source_group_dir_OUTPUT_LIST_NAME}" STREQUAL "")
+        set(enable_output_name TRUE)
+    endif()
+
+    set(enable_output_path FALSE)
+    if(NOT "${source_group_dir_OUTPUT_LIST_PATH}" STREQUAL "")
+        set(enable_output_path TRUE)
+    endif()
+
+    set(output_list_name "")
+    set(output_list_path "")
+
+    source_group_dir_recv(${prefix_name} ${dir} 
+        ${enable_output_name} ${enable_output_path}
+        output_list_name output_list_path)
+
+    if(enable_output_name)
+        set(${source_group_dir_OUTPUT_LIST_NAME} "${output_list_name}" PARENT_SCOPE)
+    endif()
+    if(enable_output_path)
+        set(${source_group_dir_OUTPUT_LIST_PATH} "${output_list_path}" PARENT_SCOPE)
+    endif()
+
+endfunction(source_group_dir)

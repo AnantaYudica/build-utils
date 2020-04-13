018332da0ee7a7c8db03547dae592dcf07eb1ecf
Ananta Yudica <nta040689@gmail.com>
2020-04-14 04:01:12 +0700

    Added arguments base dir, relative path and name

--- include/source_group_dir/condition.cmake
+++ include/source_group_dir/condition.cmake
@@ -1,3 +1,5 @@
 function(source_group_dir_condition path output)
+    cmake_parse_arguments(get_test_target_name_set_name "" 
+        "BASE_DIR;RELATIVE_PATH;NAME" "" ${ARGN}) 
     set(${output} TRUE PARENT_SCOPE)
 endfunction(source_group_dir_condition)

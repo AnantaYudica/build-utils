62732ff77b8434b81afcad135732b3870c49f328
Ananta Yudica <nta040689@gmail.com>
2020-04-14 04:05:57 +0700

    Updated prefix name for call cmake_parse_arguments function

--- include/source_group_dir/condition.cmake
+++ include/source_group_dir/condition.cmake
@@ -1,5 +1,5 @@
 function(source_group_dir_condition path output)
-    cmake_parse_arguments(get_test_target_name_set_name "" 
+    cmake_parse_arguments(source_group_dir_condition "" 
         "BASE_DIR;RELATIVE_PATH;NAME" "" ${ARGN}) 
     set(${output} TRUE PARENT_SCOPE)
 endfunction(source_group_dir_condition)
--- include/source_group_dir/filter.cmake
+++ include/source_group_dir/filter.cmake
@@ -1,5 +1,5 @@
 function(source_group_dir_filter path output)
-    cmake_parse_arguments(get_test_target_name_set_name "" 
+    cmake_parse_arguments(source_group_dir_filter "" 
         "LEVEL;BASE_DIR;RELATIVE_PATH;NAME" "" ${ARGN}) 
     set(${output} TRUE PARENT_SCOPE)
 endfunction(source_group_dir_filter)

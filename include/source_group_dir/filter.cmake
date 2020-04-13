4dbd2838c99acadabd514287b741ee695ff6ed10
Ananta Yudica <nta040689@gmail.com>
2020-04-14 04:00:53 +0700

    Updated argument level as optional argument Added arguments base dir, relative path and name

--- include/source_group_dir/filter.cmake
+++ include/source_group_dir/filter.cmake
@@ -1,3 +1,5 @@
-function(source_group_dir_filter path level output)
+function(source_group_dir_filter path output)
+    cmake_parse_arguments(get_test_target_name_set_name "" 
+        "LEVEL;BASE_DIR;RELATIVE_PATH;NAME" "" ${ARGN}) 
     set(${output} TRUE PARENT_SCOPE)
 endfunction(source_group_dir_filter)

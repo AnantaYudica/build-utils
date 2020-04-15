6559ff7a3d7d1b0eb9a6150ed6e4f69103347daf
Ananta Yudica <nta040689@gmail.com>
2020-04-16 04:19:52 +0700

    Added functions 'get_test_name_tag_ext_file_get_name', 'get_test_name_tag_ext_file_get_tag' and 'get_test_name_tag_ext_file_tag_condition' for default support function of get_test_name_tag_ext_file

---
+++ include/get_test_name_tag_ext_file/get_tag.cmake
@@ -0,0 +1,13 @@
+function(get_test_name_tag_ext_file_get_tag filename index length 
+    delim_index delim_length output_str output_index output_length)
+
+    set(one_options "STRING;CHARACTER;DELIMITER;DELIMITER_LENGTH;EXT_DELIMITER_INDEX")
+    string(CONCAT one_options "${one_options}" ";EXT_DELIMITER_LENGTH;EXT_INDEX;EXT_LENGTH")
+    cmake_parse_arguments(get_test_name_tag_ext_file_get_tag "" "${one_options}" "" ${ARGN}) 
+    
+    set(${output_index} ${index} PARENT_SCOPE)
+    set(${output_length} ${length} PARENT_SCOPE)
+
+    set(${output_str} "${get_test_name_tag_ext_file_get_tag_STRING}" PARENT_SCOPE)
+    
+endfunction(get_test_name_tag_ext_file_get_tag )

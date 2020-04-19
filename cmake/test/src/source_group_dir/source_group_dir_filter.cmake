69c70ed492be08e9758a3aca7cb606842e2998d0
Ananta Yudica <nta040689@gmail.com>
2020-04-20 06:09:35 +0700

    Added argument flag ARGS in function source_group_dir_filter Added argument flag level in argument ARGS Added variable 'level' with default value is 2

--- cmake/test/src/source_group_dir/source_group_dir_filter.cmake
+++ cmake/test/src/source_group_dir/source_group_dir_filter.cmake
@@ -1,7 +1,15 @@
 function(source_group_dir_filter path output)
     cmake_parse_arguments(source_group_dir_filter "" 
-        "LEVEL;BASE_DIR;RELATIVE_PATH;NAME" "" ${ARGN}) 
-    if (${source_group_dir_filter_LEVEL} LESS 2)
+        "LEVEL;BASE_DIR;RELATIVE_PATH;NAME" "ARGS" ${ARGN}) 
+
+    cmake_parse_arguments(args "" "level" "" ${source_group_dir_filter_ARGS})
+    
+    set(level 2)
+    if (NOT "${args_level}" STREQUAL "")
+        set(level ${args_level})
+    endif()
+
+    if (${source_group_dir_filter_LEVEL} LESS ${level})
         set(${output} TRUE PARENT_SCOPE)
     else()
         set(${output} FALSE PARENT_SCOPE)

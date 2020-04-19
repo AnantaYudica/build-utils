5ac0a8b3fd1dec0f4b1c4dc0b5f03b402dd04aea
Ananta Yudica <nta040689@gmail.com>
2020-04-20 06:11:38 +0700

    Added argument flag ARGS in function source_group_dir_filter Added argument flags name and relative_path_regex in argument ARGS Added variable 'name' with default value is test1.txt Added variable 'regex' with default value is ^[^b]*$

--- cmake/test/src/source_group_dir/source_group_dir_condition.cmake
+++ cmake/test/src/source_group_dir/source_group_dir_condition.cmake
@@ -1,9 +1,21 @@
 function(source_group_dir_condition path output)
     cmake_parse_arguments(source_group_dir_condition "" 
-        "BASE_DIR;RELATIVE_PATH;NAME" "" ${ARGN}) 
-   
-    if ("${source_group_dir_condition_NAME}" STREQUAL "test1.txt"
-        AND (NOT "${source_group_dir_condition_RELATIVE_PATH}" MATCHES ".*b.*"))
+        "BASE_DIR;RELATIVE_PATH;NAME" "ARGS" ${ARGN}) 
+
+    cmake_parse_arguments(args "" "name;relative_path_regex" "" ${source_group_dir_condition_ARGS})
+
+    set(name "test1.txt")
+    if (NOT "${args_name}" STREQUAL "")
+        set(name "${args_name}")
+    endif()
+
+    set(regex "^[^b]*$")
+    if (NOT "${args_relative_path_regex}" STREQUAL "")
+        set(regex "${args_relative_path_regex}")
+    endif()
+
+    if ("${source_group_dir_condition_NAME}" STREQUAL "${name}"
+        AND ("${source_group_dir_condition_RELATIVE_PATH}" MATCHES "${regex}"))
         
         set(${output} TRUE PARENT_SCOPE)
     else()

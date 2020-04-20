a1dcb8dfa95725f4ff60d3386d1120134c32ffbe
Ananta Yudica <nta040689@gmail.com>
2020-04-20 08:21:24 +0700

    Added argument flag ARGS in function get_test_source_dir_condition Added argument flags name and relative_path_regex in argument ARGS Added variable 'name' with default value is test1.txt Added variable 'regex' with default value is ^[^b]*$

--- cmake/test/src/get_test_source_dir/get_test_source_dir_condition.cmake
+++ cmake/test/src/get_test_source_dir/get_test_source_dir_condition.cmake
@@ -1,8 +1,21 @@
 function(get_test_source_dir_condition path output)
     cmake_parse_arguments(get_test_source_dir_condition "" 
-        "BASE_DIR;RELATIVE_PATH;NAME" "" ${ARGN}) 
-    if ("${get_test_source_dir_condition_NAME}" STREQUAL "test1.txt"
-        AND (NOT "${get_test_source_dir_condition_RELATIVE_PATH}" MATCHES ".*b.*"))
+        "BASE_DIR;RELATIVE_PATH;NAME" "ARGS" ${ARGN}) 
+
+    cmake_parse_arguments(args "" "name;relative_path_regex" "" ${get_test_source_dir_condition_ARGS})
+
+    set(name "test1.txt")
+    if (NOT "${args_name}" STREQUAL "")
+        set(name "${args_name}")
+    endif()
+
+    set(regex "^[^b]*$")
+    if (NOT "${args_relative_path_regex}" STREQUAL "")
+        set(regex "${args_relative_path_regex}")
+    endif()
+
+    if ("${get_test_source_dir_condition_NAME}" STREQUAL "${name}"
+        AND ("${get_test_source_dir_condition_RELATIVE_PATH}" MATCHES "${regex}"))
         
         set(${output} TRUE PARENT_SCOPE)
     else()

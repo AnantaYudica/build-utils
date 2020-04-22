b1c2a8af29c9a2f5353333f2ca78a0aed8c55bac
Ananta Yudica <nta040689@gmail.com>
2020-04-22 18:07:54 +0700

    Added CASE_SENSITIVE in boolean options of function get_test_source_dir_condition and get_test_source_dir_filter

--- include/get_test_source_dir/condition.cmake
+++ include/get_test_source_dir/condition.cmake
@@ -1,5 +1,5 @@
 function(get_test_source_dir_condition path output)
-    cmake_parse_arguments(get_test_source_dir_condition "" 
+    cmake_parse_arguments(get_test_source_dir_condition "CASE_SENSITIVE" 
         "BASE_DIR;RELATIVE_PATH;FILENAME;INCLUDE_DIR" "ARGS" ${ARGN}) 
     set(${output} TRUE PARENT_SCOPE)
 endfunction(get_test_source_dir_condition)
--- include/get_test_source_dir/filter.cmake
+++ include/get_test_source_dir/filter.cmake
@@ -1,5 +1,5 @@
 function(get_test_source_dir_filter path output)
-    cmake_parse_arguments(get_test_source_dir_filter "" 
+    cmake_parse_arguments(get_test_source_dir_filter "CASE_SENSITIVE" 
         "LEVEL;BASE_DIR;RELATIVE_PATH;FILENAME;INCLUDE_DIR" "ARGS" ${ARGN}) 
     set(${output} TRUE PARENT_SCOPE)
 endfunction(get_test_source_dir_filter)

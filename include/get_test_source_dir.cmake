20e29a406f7f37ab06506c056941adba9b668477
Ananta Yudica <nta040689@gmail.com>
2020-04-22 18:10:27 +0700

    Added argument flag CASE_SENSITIVE in function get_test_source_dir Added argument is_case_sensitive in function get_test_source_dir_recv

--- include/get_test_source_dir.cmake
+++ include/get_test_source_dir.cmake
@@ -1,4 +1,5 @@
-function(get_test_source_dir_recv base_dir dir list_output is_recursive level)
+function(get_test_source_dir_recv base_dir dir list_output is_recursive 
+    is_case_sensitive level)
     cmake_parse_arguments(get_test_source_dir_recv "" "INCLUDE_DIR" 
         "FILTER_ARGS;CONDITION_ARGS" ${ARGN}) 
 
@@ -7,26 +8,32 @@ function(get_test_source_dir_recv base_dir dir list_output is_recursive level)
     set(include_dir "${get_test_source_dir_recv_INCLUDE_DIR}")
 
     file(GLOB list_path "${dir}/*")
+    
+    set(case_sensitive_arg "")
+    if (is_case_sensitive)
+        set(case_sensitive_arg "CASE_SENSITIVE")
+    endif()
+
     set(foreach_list_path "")
     foreach(it ${list_path})
         file(RELATIVE_PATH relative_dir "${base_dir}" "${it}")
         get_filename_component(file_name ${it} NAME)
         get_test_source_dir_filter(${it} ok LEVEL ${level} BASE_DIR ${base_dir} 
             RELATIVE_PATH ${relative_dir} FILENAME ${file_name}
-            ARGS ${filter_args} INCLUDE_DIR ${include_dir})
+            ARGS ${filter_args} INCLUDE_DIR ${include_dir} ${case_sensitive_arg})
         if (ok)
             if(IS_DIRECTORY ${it} AND (is_recursive))
                 set(next_list_path "")
                 math(EXPR next_level "${level}+1")
                 get_test_source_dir_recv("${base_dir}" "${dir}/${file_name}" next_list_path
-                    ${is_recursive} ${next_level} 
+                    ${is_recursive} ${is_case_sensitive} ${next_level} 
                     FILTER_ARGS ${filter_args} CONDITION_ARGS ${condition_args}
                     INCLUDE_DIR ${include_dir})
                 list(APPEND foreach_list_path ${next_list_path})
             elseif(NOT IS_DIRECTORY ${it})
                 get_test_source_dir_condition(${it} ok BASE_DIR ${base_dir} 
                     RELATIVE_PATH ${relative_dir} FILENAME ${file_name}
-                    ARGS ${condition_args} INCLUDE_DIR ${include_dir})
+                    ARGS ${condition_args} INCLUDE_DIR ${include_dir} ${case_sensitive_arg})
                 if (ok)
                     list(APPEND foreach_list_path ${it})
                 endif()
@@ -38,7 +45,7 @@ endfunction(get_test_source_dir_recv)
 
 
 function(get_test_source_dir dir list_output)
-    cmake_parse_arguments(get_test_source_dir "RECURSIVE" 
+    cmake_parse_arguments(get_test_source_dir "RECURSIVE;CASE_SENSITIVE" 
         "FILTER;CONDITION;INCLUDE_DIR" 
         "FILTER_ARGS;CONDITION_ARGS" ${ARGN}) 
 
@@ -65,7 +72,8 @@ function(get_test_source_dir dir list_output)
     set(output_list_path "")
 
     get_test_source_dir_recv("${dir}" "${dir}" output_list_path
-        ${get_test_source_dir_RECURSIVE} 0 
+        ${get_test_source_dir_RECURSIVE} 
+        ${get_test_source_dir_CASE_SENSITIVE} 0 
         FILTER_ARGS ${filter_args} CONDITION_ARGS ${condition_args}
         INCLUDE_DIR ${base_dir})
 

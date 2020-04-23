ab2a0b5de783a59e523692cba296f1899484971a
Ananta Yudica <nta040689@gmail.com>
2020-04-23 10:32:13 +0700

    Added boolean option CASE_SENSITIVE

--- include/add_test_executable_dir/main_header_filter.cmake
+++ include/add_test_executable_dir/main_header_filter.cmake
@@ -1,16 +1,23 @@
 function(get_test_source_dir_filter path output)
-    cmake_parse_arguments(get_test_source_dir_filter "" 
+    cmake_parse_arguments(get_test_source_dir_filter "CASE_SENSITIVE" 
         "LEVEL;BASE_DIR;RELATIVE_PATH;FILENAME;INCLUDE_DIR" "ARGS" ${ARGN}) 
 
     set(filename "${get_test_source_dir_filter_FILENAME}")
     
+    set(is_case_sensitive FALSE)
+    if (NOT "${get_test_source_dir_filter_CASE_SENSITIVE}" STREQUAL "")
+        set(is_case_sensitive ${get_test_source_dir_filter_CASE_SENSITIVE})
+    endif()
+
     cmake_parse_arguments(args "" "" "LIST_EXT")
 
     set(list_ext ${args_LIST_EXT})
 
     get_filename_component(filename_ext ${it} EXT)
 
-    string(TOLOWER filename_ext "${filename_ext}")
+    if (NOT is_case_sensitive)
+        string(TOLOWER "${filename_ext}" filename_ext)
+    endif()
 
     set(result FALSE)
     if (NOT "${list_ext}" STREQUAL "")
@@ -23,6 +30,6 @@ function(get_test_source_dir_filter path output)
     endif()
 
     set(${output} ${result} PARENT_SCOPE)
-    
+
 endfunction(get_test_source_dir_filter)
 

47754ea6afd8f8cf3be20292b434090df245eca1
Ananta Yudica <nta040689@gmail.com>
2020-04-24 11:39:23 +0700

    Added function add_test_executable_dir_get_dir_name

---
+++ include/add_test_executable_dir/get_dir_name.cmake
@@ -0,0 +1,9 @@
+function(add_test_executable_dir_get_dir_name path dir_name)
+    
+    cmake_parse_arguments(add_test_executable_dir_get_dir_name "" 
+        "BASE_DIR;RELATIVE_PATH;FILENAME;NAME;TAG;EXT;INCLUDE_DIR" 
+        "ARGS" ${ARGN}) 
+
+    set(${dir_name} "${add_test_executable_dir_get_dir_name_FILENAME}")
+
+endfunction(add_test_executable_dir_get_dir_name)

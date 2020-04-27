f821bc5e2848116bfdecb5ab5753d1c54773ea30
Ananta Yudica <nta040689@gmail.com>
2020-04-27 16:46:22 +0700

    Updated argument function name from out_libraries to out_list_library

--- include/add_test_executable_dir/get_target_link.cmake
+++ include/add_test_executable_dir/get_target_link.cmake
@@ -1,9 +1,9 @@
-function(add_test_executable_dir_get_target_link path out_libraries)
+function(add_test_executable_dir_get_target_link path out_list_library)
     
     cmake_parse_arguments(add_test_executable_dir_get_target_name "" 
         "DIR;BASE_DIR;RELATIVE_PATH;FILENAME;NAME;TAG;EXT;INCLUDE_DIR;TARGET_NAME" 
         "ARGS" ${ARGN}) 
 
-    set(${out_libraries} "" PARENT_SCOPE)
+    set(${out_list_library} "" PARENT_SCOPE)
     
 endfunction(add_test_executable_dir_get_target_link)

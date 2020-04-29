8ff8cd2d178bcbaa059a15955324c60895c7da79
Ananta Yudica <nta040689@gmail.com>
2020-04-30 04:17:23 +0700

    Added Added argument flags DEFAULT_LIST_DEFINITION, DEFAULT_LIST_OPTION, RELATIVE_DIR and TARGET_DIR Updated set variables out_list_options and out_list_definitions

--- include/add_test_executable_dir/get_target_compile.cmake
+++ include/add_test_executable_dir/get_target_compile.cmake
@@ -1,11 +1,16 @@
-function(add_test_executable_dir_get_target_compile path out_list_options
-    out_list_definitions)
+function(add_test_executable_dir_get_target_compile path out_list_definition
+    out_list_option)
     
+    string(CONCAT one_options "TARGET_NAME;TARGET_DIR"
+        ";BASE_DIR;RELATIVE_PATH;FILENAME;NAME;TAG;EXT"
+        ";DIR;RELATIVE_DIR;INCLUDE_DIR")
     cmake_parse_arguments(add_test_executable_dir_get_target_compile "" 
-        "DIR;BASE_DIR;RELATIVE_PATH;FILENAME;NAME;TAG;EXT;INCLUDE_DIR;TARGET_NAME" 
-        "ARGS" ${ARGN}) 
+        "${one_options}"
+        "DEFAULT_LIST_DEFINITION;DEFAULT_LIST_OPTION;ARGS" ${ARGN}) 
 
-    set(${out_list_options} "" PARENT_SCOPE)
-    set(${out_list_definitions} "" PARENT_SCOPE)
+    set(${out_list_definition} 
+        "${add_test_executable_dir_get_target_compile_DEFAULT_LIST_DEFINITION}" PARENT_SCOPE)
+    set(${out_list_option} 
+        "add_test_executable_dir_get_target_compile_DEFAULT_LIST_OPTION" PARENT_SCOPE)
     
 endfunction(add_test_executable_dir_get_target_compile)

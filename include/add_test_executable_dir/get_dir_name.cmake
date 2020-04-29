8bcb2306be784e9d973753f274ea14bf4fecf0c1
Ananta Yudica <nta040689@gmail.com>
2020-04-29 20:45:26 +0700

    Updated set function with add PARENT_SCOPE as argument

--- include/add_test_executable_dir/get_dir_name.cmake
+++ include/add_test_executable_dir/get_dir_name.cmake
@@ -4,6 +4,6 @@ function(add_test_executable_dir_get_dir_name path dir_name)
         "BASE_DIR;RELATIVE_PATH;FILENAME;NAME;TAG;EXT;INCLUDE_DIR" 
         "ARGS" ${ARGN}) 
 
-    set(${dir_name} "${add_test_executable_dir_get_dir_name_FILENAME}")
+    set(${dir_name} "${add_test_executable_dir_get_dir_name_FILENAME}" PARENT_SCOPE)
 
 endfunction(add_test_executable_dir_get_dir_name)

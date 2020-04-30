b5c9c6459d077aefe60bc8ab3352dda42404d2b5
Ananta Yudica <nta040689@gmail.com>
2020-05-01 01:19:56 +0700

    Fixed argument in function cmake_parse_arguments Added argument flag TARGET_NAME, TARGET_DIR, SRC_PATH, SRC_BASE_DIR, SRC_RELATIVE_PATH, SRC_FILENAME, SRC_NAME, SRC_TAG, SRC_EXT, SRC_DIR, SRC_RELATIVE_DIR and HEADER_FILTER_ARGS Update argument flag ARGS of function add_test_executable_dir_header_filter filled in before variable call_args

--- include/add_test_executable_dir/get_test_source_dir/filter.cmake
+++ include/add_test_executable_dir/get_test_source_dir/filter.cmake
@@ -2,7 +2,12 @@ function(get_test_source_dir_filter path output)
     cmake_parse_arguments(get_test_source_dir_filter "CASE_SENSITIVE" 
         "LEVEL;BASE_DIR;RELATIVE_PATH;FILENAME;INCLUDE_DIR" "ARGS" ${ARGN}) 
 
-    cmake_parse_arguments(args "" "HEADER_FILTER" "LIST_EXT")
+    string(CONCAT one_options_args "HEADER_FILTER"
+        ";TARGET_NAME;TARGET_DIR;SRC_PATH;SRC_BASE_DIR;SRC_RELATIVE_PATH"
+        ";SRC_FILENAME;SRC_NAME;SRC_TAG;SRC_EXT;SRC_DIR;SRC_RELATIVE_DIR")
+    cmake_parse_arguments(args "" 
+        "${one_options_args}" "LIST_EXT;HEADER_FILTER_ARGS"
+        ${get_test_source_dir_filter_ARGS})
 
     set(base_dir "${BUILD_UTILS_INCLUDE_DIR}")
     if (NOT "${get_test_source_dir_filter_INCLUDE_DIR}" STREQUAL "")
@@ -27,13 +32,57 @@ function(get_test_source_dir_filter path output)
         list(APPEND call_args "LIST_TAG" ${args_LIST_TAG})
     endif()
 
+    if(NOT "${args_TARGET_NAME}" STREQUAL "")
+        list(APPEND call_args "TARGET_NAME" ${args_TARGET_NAME})
+    endif()
+
+    if(NOT "${args_TARGET_DIR}" STREQUAL "")
+        list(APPEND call_args "TARGET_DIR" ${args_TARGET_DIR})
+    endif()
+
+    if(NOT "${args_SRC_PATH}" STREQUAL "")
+        list(APPEND call_args "SRC_PATH" ${args_SRC_PATH})
+    endif()
+
+    if(NOT "${args_SRC_BASE_DIR}" STREQUAL "")
+        list(APPEND call_args "SRC_BASE_DIR" ${args_SRC_BASE_DIR})
+    endif()
+
+    if(NOT "${args_SRC_RELATIVE_PATH}" STREQUAL "")
+        list(APPEND call_args "SRC_RELATIVE_PATH" ${args_SRC_RELATIVE_PATH})
+    endif()
+    
+    if(NOT "${args_SRC_FILENAME}" STREQUAL "")
+        list(APPEND call_args "SRC_FILENAME" ${args_SRC_FILENAME})
+    endif()
+
+    if(NOT "${args_SRC_NAME}" STREQUAL "")
+        list(APPEND call_args "SRC_NAME" ${args_SRC_NAME})
+    endif()
+
+    if(NOT "${args_SRC_TAG}" STREQUAL "")
+        list(APPEND call_args "SRC_TAG" ${args_SRC_TAG})
+    endif()
+
+    if(NOT "${args_SRC_EXT}" STREQUAL "")
+        list(APPEND call_args "SRC_EXT" ${args_SRC_EXT})
+    endif()
+
+    if(NOT "${args_SRC_DIR}" STREQUAL "")
+        list(APPEND call_args "SRC_DIR" ${args_SRC_DIR})
+    endif()
+
+    if(NOT "${args_SRC_RELATIVE_DIR}" STREQUAL "")
+        list(APPEND call_args "SRC_RELATIVE_DIR" ${args_SRC_RELATIVE_DIR})
+    endif()
+    
     add_test_executable_dir_header_filter(${path} result ${case_sensitive_arg} 
         LEVEL ${get_test_source_dir_filter_LEVEL}
         BASE_DIR ${get_test_source_dir_filter_BASE_DIR}
         RELATIVE_PATH ${get_test_source_dir_filter_RELATIVE_PATH}
         FILENAME ${get_test_source_dir_filter_FILENAME}
         INCLUDE_DIR ${get_test_source_dir_filter_INCLUDE_DIR}
-        ARGS ${call_args})
+        ${call_args} ARGS ${args_HEADER_FILTER_ARGS})
 
     set(${output} ${result} PARENT_SCOPE)
 

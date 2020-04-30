b4c662eadd4479b6e6d81bdf5a1fc2a79b7176ea
Ananta Yudica <nta040689@gmail.com>
2020-04-30 17:30:08 +0700

    Added argument flag RELATIVE_DIR Updated set value flag of variable ref out_target_name with PARENT_SCOPE

--- include/add_test_executable_dir/get_target_name.cmake
+++ include/add_test_executable_dir/get_target_name.cmake
@@ -1,8 +1,10 @@
 function(add_test_executable_dir_get_target_name path out_target_name)
     
+    string(CONCAT one_options "BASE_DIR;RELATIVE_PATH;FILENAME;NAME;TAG;EXT"
+        ";DIR;RELATIVE_DIR;INCLUDE_DIR")
     cmake_parse_arguments(add_test_executable_dir_get_target_name "" 
-        "DIR;BASE_DIR;RELATIVE_PATH;FILENAME;NAME;TAG;EXT;INCLUDE_DIR" "ARGS" ${ARGN}) 
+        "${one_options}" "ARGS" ${ARGN}) 
 
-    set(${out_target_name} "${add_test_executable_dir_get_target_name_NAME}")
+    set(${out_target_name} "${add_test_executable_dir_get_target_name_NAME}" PARENT_SCOPE)
 
 endfunction(add_test_executable_dir_get_target_name)

3d924d1f6520ad186f09cc0078965c41b53d181f
Ananta Yudica <nta040689@gmail.com>
2020-04-30 16:23:59 +0700

    Added argument flags DEFAULT_LIST_DIR, RELATIVE_DIR and TARGET_DIR

--- include/add_test_executable_dir/get_target_include.cmake
+++ include/add_test_executable_dir/get_target_include.cmake
@@ -1,9 +1,12 @@
-function(add_test_executable_dir_get_target_include path out_list_directory)
+function(add_test_executable_dir_get_target_include path out_list_dir)
     
+    string(CONCAT one_options "TARGET_NAME;TARGET_DIR"
+        ";BASE_DIR;RELATIVE_PATH;FILENAME;NAME;TAG;EXT"
+        ";DIR;RELATIVE_DIR;INCLUDE_DIR"
     cmake_parse_arguments(add_test_executable_dir_get_target_include "" 
-        "DIR;BASE_DIR;RELATIVE_PATH;FILENAME;NAME;TAG;EXT;INCLUDE_DIR;TARGET_NAME" 
-        "ARGS" ${ARGN}) 
+        "${one_options}" 
+        "ARGS;DEFAULT_LIST_DIR" ${ARGN}) 
 
-    set(${out_list_directory} "" PARENT_SCOPE)
+    set(${out_list_dir} "add_test_executable_dir_get_target_include_DEFAULT_LIST_DIR" PARENT_SCOPE)
     
 endfunction(add_test_executable_dir_get_target_include)

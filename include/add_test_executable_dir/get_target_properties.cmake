07b079c3003dedbb53b7ddceeb2f8e899e2a30a0
Ananta Yudica <nta040689@gmail.com>
2020-04-30 19:56:27 +0700

    Updated argument name from out_args to out_list_property Added argument flags DEFAULT_LIST_PROPERTY, RELATIVE_DIR and TARGET_DIR Updated set variable of ref out_list_property

--- include/add_test_executable_dir/get_target_properties.cmake
+++ include/add_test_executable_dir/get_target_properties.cmake
@@ -1,9 +1,12 @@
-function(add_test_executable_dir_get_target_properties path out_args)
+function(add_test_executable_dir_get_target_properties path out_list_property)
     
+    string(CONCAT one_options "TARGET_NAME;TARGET_DIR"
+        ";BASE_DIR;RELATIVE_PATH;FILENAME;NAME;TAG;EXT"
+        ";DIR;RELATIVE_DIR;INCLUDE_DIR")
     cmake_parse_arguments(add_test_executable_dir_get_target_properties "" 
-        "DIR;BASE_DIR;RELATIVE_PATH;FILENAME;NAME;TAG;EXT;INCLUDE_DIR;TARGET_NAME" 
-        "ARGS" ${ARGN}) 
+        "${one_options}" 
+        "ARGS;DEFAULT_LIST_PROPERTY" ${ARGN}) 
 
-    set(${out_args} "" PARENT_SCOPE)
+    set(${out_list_property} "${add_test_executable_dir_get_target_properties_DEFAULT_LIST_PROPERTY}" PARENT_SCOPE)
     
 endfunction(add_test_executable_dir_get_target_properties)

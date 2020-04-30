a2d2e2baadd0372a92d5e62abfe51340965a4969
Ananta Yudica <nta040689@gmail.com>
2020-04-30 19:45:19 +0700

    Added argument flags DEFAULT_LIST_SRC, RELATIVE_DIR and TARGET_DIR Updated set variable of ref out_list_src

--- include/add_test_executable_dir/get_target_other_src.cmake
+++ include/add_test_executable_dir/get_target_other_src.cmake
@@ -1,9 +1,12 @@
 function(add_test_executable_dir_get_target_other_src path out_list_src)
     
+    string(CONCAT one_options "TARGET_NAME;TARGET_DIR"
+        ";BASE_DIR;RELATIVE_PATH;FILENAME;NAME;TAG;EXT"
+        ";DIR;RELATIVE_DIR;INCLUDE_DIR")
     cmake_parse_arguments(add_test_executable_dir_get_target_other_src "" 
-        "DIR;BASE_DIR;RELATIVE_PATH;FILENAME;NAME;TAG;EXT;INCLUDE_DIR;TARGET_NAME" 
-        "ARGS" ${ARGN}) 
+        "${one_options}" 
+        "ARGS;DEFAULT_LIST_SRC" ${ARGN}) 
 
-    set(${out_list_src} "" PARENT_SCOPE)
+    set(${out_list_src} "${add_test_executable_dir_get_target_other_src_DEFAULT_LIST_SRC}" PARENT_SCOPE)
     
 endfunction(add_test_executable_dir_get_target_other_src)

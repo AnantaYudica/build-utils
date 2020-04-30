2bd9a9459c63199354a8bae00cdcd46bc1c8c836
Ananta Yudica <nta040689@gmail.com>
2020-04-30 15:56:50 +0700

    Updated argument name from path and name to dir and out_dir_name Updated argument flags RELATIVE_PATH and FILENAME to RELATIVE_DIR and DIR_NAME Removed argument flag EXT

--- include/add_test_executable_dir/get_target_dir.cmake
+++ include/add_test_executable_dir/get_target_dir.cmake
@@ -1,9 +1,9 @@
-function(add_test_executable_dir_get_target_dir path name)
+function(add_test_executable_dir_get_target_dir dir out_dir_name)
     
     cmake_parse_arguments(add_test_executable_dir_get_target_dir "" 
-        "BASE_DIR;RELATIVE_PATH;FILENAME;NAME;TAG;EXT;INCLUDE_DIR" 
+        "BASE_DIR;RELATIVE_DIR;DIR_NAME;NAME;TAG;INCLUDE_DIR" 
         "ARGS" ${ARGN}) 
 
-    set(${name} "${add_test_executable_dir_get_target_dir_FILENAME}")
+    set(${out_dir_name} "${add_test_executable_dir_get_target_dir_DIR_NAME}")
 
 endfunction(add_test_executable_dir_get_target_dir)

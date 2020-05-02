7995376201402d8613ae052affef21518f7dbcc1
Ananta Yudica <nta040689@gmail.com>
2020-05-03 05:10:50 +0700

    moved semicolon

--- include/source_group_dir/filter.cmake
+++ include/source_group_dir/filter.cmake
@@ -1,6 +1,6 @@
 function(source_group_dir_filter output)
-    string(CONCAT one_options "LEVEL;BASE_DIR;PATH;RELATIVE_PATH;FILENAME;DIRNAME;"
-        "CURR_DIR;RELATIVE_CURR_DIR;CURR_DIRNAME;INCLUDE_DIR")
+    string(CONCAT one_options "LEVEL;BASE_DIR;PATH;RELATIVE_PATH;FILENAME;DIRNAME"
+        ";CURR_DIR;RELATIVE_CURR_DIR;CURR_DIRNAME;INCLUDE_DIR")
     cmake_parse_arguments(source_group_dir_filter "CASE_SENSITIVE;DIRECTORY" 
         "${one_options}" "ARGS" ${ARGN}) 
     set(${output} TRUE PARENT_SCOPE)

2624308faf6bdbd48496c0c170b09cacf436f79b
Ananta Yudica <nta040689@gmail.com>
2020-05-03 01:52:39 +0700

    Removed argument name 'path' Added argument flag 'PATH', 'DIRNAME', 'CURR_DIR', 'RELATIVE_CURR_DIR' and 'CURR_DIRNAME'

--- include/source_group_dir/filter.cmake
+++ include/source_group_dir/filter.cmake
@@ -1,5 +1,7 @@
-function(source_group_dir_filter path output)
-    cmake_parse_arguments(source_group_dir_filter "CASE_SENSITIVE" 
-        "LEVEL;BASE_DIR;RELATIVE_PATH;FILENAME;INCLUDE_DIR" "ARGS" ${ARGN}) 
+function(source_group_dir_filter output)
+    string(CONCAT one_options "LEVEL;BASE_DIR;PATH;RELATIVE_PATH;FILENAME;DIRNAME;"
+        "CURR_DIR;RELATIVE_CURR_DIR;CURR_DIRNAME;INCLUDE_DIR")
+    cmake_parse_arguments(source_group_dir_filter "CASE_SENSITIVE;DIRECTORY" 
+        "${one_options}" "ARGS" ${ARGN}) 
     set(${output} TRUE PARENT_SCOPE)
 endfunction(source_group_dir_filter)

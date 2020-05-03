97ad99d782cc538d739dd7398b46d7a687af04f1
Ananta Yudica <nta040689@gmail.com>
2020-05-03 20:42:02 +0700

    Added argument flag PATH, CURR_DIR, RELATIVE_CURR_DIR and CURR_DIRNAME Removed argument name path Rename variable name from filename to dirname

--- include/source_group_dir/get_prefix_name.cmake
+++ include/source_group_dir/get_prefix_name.cmake
@@ -1,10 +1,10 @@
-function(source_group_dir_get_prefix_name path current_prefix_name prefix_name)
+function(source_group_dir_get_prefix_name current_prefix_name prefix_name)
+    string(CONCAT one_options "LEVEL;BASE_DIR;PATH;RELATIVE_PATH;DIRNAME"
+        ";CURR_DIR;RELATIVE_CURR_DIR;CURR_DIRNAME;INCLUDE_DIR")
     cmake_parse_arguments(source_group_dir_get_prefix_name "" 
-        "LEVEL;BASE_DIR;RELATIVE_PATH;FILENAME;INCLUDE_DIR" "ARGS" ${ARGN}) 
+        "${one_options}" "ARGS" ${ARGN}) 
     
-    set(filename "${source_group_dir_get_prefix_name_FILENAME}")
-    
-    set(${prefix_name} "${current_prefix_name}/${filename}" PARENT_SCOPE)
-    
+    set(dirname "${source_group_dir_get_prefix_name_DIRNAME}")
+    set(${prefix_name} "${current_prefix_name}/${dirname}" PARENT_SCOPE)
 endfunction(source_group_dir_get_prefix_name)
 

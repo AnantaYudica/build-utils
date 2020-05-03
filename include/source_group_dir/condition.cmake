2e9f0dd695f9f5decff43567a66c39728e51ef4f
Ananta Yudica <nta040689@gmail.com>
2020-05-03 22:32:28 +0700

    Fixed semicolon

--- include/source_group_dir/condition.cmake
+++ include/source_group_dir/condition.cmake
@@ -1,6 +1,6 @@
 function(source_group_dir_condition output)
     string(CONCAT one_options "BASE_DIR;RELATIVE_PATH;PATH;FILENAME"
-        "CURR_DIR;RELATIVE_CURR_DIR;CURR_DIRNAME;INCLUDE_DIR")
+        ";CURR_DIR;RELATIVE_CURR_DIR;CURR_DIRNAME;INCLUDE_DIR")
     cmake_parse_arguments(source_group_dir_condition "CASE_SENSITIVE" 
         "${one_options}" "ARGS" ${ARGN}) 
     set(${output} TRUE PARENT_SCOPE)

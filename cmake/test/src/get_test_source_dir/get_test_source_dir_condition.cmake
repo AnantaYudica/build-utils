50d62491c757dda3c7f61579b40c64bd81987e00
Ananta Yudica <nta040689@gmail.com>
2020-05-04 16:56:44 +0700

    Updated implementation of function get_test_source_dir_condition and get_test_source_dir_filter

--- cmake/test/src/get_test_source_dir/get_test_source_dir_condition.cmake
+++ cmake/test/src/get_test_source_dir/get_test_source_dir_condition.cmake
@@ -1,6 +1,8 @@
-function(get_test_source_dir_condition path output)
+function(get_test_source_dir_condition output)
+    string(CONCAT one_options "BASE_DIR;PATH;RELATIVE_PATH;FILENAME"
+        ";CURR_DIR;RELATIVE_CURR_DIR;CURR_DIRNAME;INCLUDE_DIR") 
     cmake_parse_arguments(get_test_source_dir_condition "CASE_SENSITIVE" 
-        "BASE_DIR;RELATIVE_PATH;FILENAME" "ARGS" ${ARGN}) 
+        "${one_options}" "ARGS" ${ARGN}) 
     
     set(is_case_sensitive FALSE)
     if (NOT "${get_test_source_dir_condition_CASE_SENSITIVE}" STREQUAL "")

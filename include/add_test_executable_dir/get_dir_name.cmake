b86713247ad541905b144f463dcb34a53833b526
Ananta Yudica <nta040689@gmail.com>
2020-05-06 00:52:59 +0700

    Removed argument name dir Renamed argument name from out_dir_name to output Renamed argument option flag from RELATIVE_DIR and DIR_NAME to RELATIVE_PATH and DIRNAME Added argument option flag PATH

--- include/add_test_executable_dir/get_dir_name.cmake
+++ include/add_test_executable_dir/get_dir_name.cmake
@@ -1,9 +1,9 @@
-function(add_test_executable_dir_get_dir_name dir out_dir_name)
+function(add_test_executable_dir_get_dir_name output)
     
     cmake_parse_arguments(add_test_executable_dir_get_dir_name "" 
-        "BASE_DIR;RELATIVE_DIR;DIR_NAME;NAME;TAG;INCLUDE_DIR" 
+        "BASE_DIR;PATH;RELATIVE_PATH;DIRNAME;NAME;TAG;INCLUDE_DIR" 
         "ARGS" ${ARGN}) 
 
-    set(${out_dir_name} "${add_test_executable_dir_get_dir_name_DIR_NAME}" PARENT_SCOPE)
+    set(${output} "${add_test_executable_dir_get_dir_name_DIRNAME}" PARENT_SCOPE)
 
 endfunction(add_test_executable_dir_get_dir_name)

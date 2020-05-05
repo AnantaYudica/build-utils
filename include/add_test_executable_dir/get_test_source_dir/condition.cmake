5dffbb94e61b590bc22ae1ed138f61a5ce0871bf
Ananta Yudica <nta040689@gmail.com>
2020-05-06 03:40:23 +0700

    Added argument flag PATH, CURR_DIR, RELATIVE_CURR_DIR and CURR_DIRNAME Renamed argument flag from SRC_DIR and SRC_RELATIVE_DIR to SRC_CURR_DIR and SRC_RELATIVE_CURR_DIR in ARGS Added argument flag SRC_CURR_DIRNAME in ARGS Removed argument flag LIST_TAG in ARGS Removed argument name path Added condition not COMMAND add_test_executable_dir_header_condition for check command add_test_executable_dir_header_condition is available Updated call function add_test_executable_dir_header_condition

--- include/add_test_executable_dir/get_test_source_dir/condition.cmake
+++ include/add_test_executable_dir/get_test_source_dir/condition.cmake
@@ -1,25 +1,29 @@
-function(get_test_source_dir_condition path output)
+function(get_test_source_dir_condition output)
+    string(CONCAT one_options "BASE_DIR;PATH;RELATIVE_PATH;FILENAME"
+        ";CURR_DIR;RELATIVE_CURR_DIR;CURR_DIRNAME;INCLUDE_DIR") 
     cmake_parse_arguments(get_test_source_dir_condition "CASE_SENSITIVE" 
-        "BASE_DIR;RELATIVE_PATH;FILENAME;INCLUDE_DIR" "ARGS" ${ARGN}) 
+        "${one_options}" "ARGS" ${ARGN}) 
 
     string(CONCAT one_options_args "HEADER_CONDITION;GET_NAME;GET_TAG;TAG_CONDITION;TAG_DELIMITER"
         ";TARGET_NAME;TARGET_DIR;SRC_PATH;SRC_BASE_DIR;SRC_RELATIVE_PATH"
-        ";SRC_FILENAME;SRC_NAME;SRC_TAG;SRC_EXT;SRC_DIR;SRC_RELATIVE_DIR")
-    cmake_parse_arguments(args "" 
-        "${one_options_args}" 
-        "LIST_TAG;HEADER_CONDITION_ARGS" ${get_test_source_dir_condition_ARGS})
+        ";SRC_FILENAME;SRC_NAME;SRC_TAG;SRC_EXT;SRC_CURR_DIR"
+        ";SRC_RELATIVE_CURR_DIR;SRC_CURR_DIRNAME")
+    cmake_parse_arguments(args "" "${one_options_args}" 
+        "HEADER_CONDITION_ARGS" ${get_test_source_dir_condition_ARGS})
 
     set(base_dir "${BUILD_UTILS_INCLUDE_DIR}")
     if (NOT "${get_test_source_dir_condition_INCLUDE_DIR}" STREQUAL "")
         set(base_dir "${get_test_source_dir_condition_INCLUDE_DIR}")
     endif()
 
-    if("${args_HEADER_CONDITION}" STREQUAL "" 
-        OR (NOT EXISTS "${args_HEADER_CONDITION}"))
+    if (NOT COMMAND add_test_executable_dir_header_condition)
+        if("${args_HEADER_CONDITION}" STREQUAL "" 
+            OR (NOT EXISTS "${args_HEADER_CONDITION}"))
 
-        include(${base_dir}/add_test_executable_dir/header_condition.cmake)
-    else()
-        include(${args_HEADER_CONDITION})
+            include(${base_dir}/add_test_executable_dir/header_condition/default.cmake)
+        else()
+            include(${args_HEADER_CONDITION})
+        endif()
     endif()
 
     set(case_sensitive_arg "")
@@ -43,10 +47,6 @@ function(get_test_source_dir_condition path output)
     if (NOT "${args_TAG_DELIMITER}" STREQUAL "")
         list(APPEND call_args "TAG_DELIMITER" ${args_TAG_DELIMITER})
     endif() 
-
-    if (NOT "${args_LIST_TAG}" STREQUAL "")
-        list(APPEND call_args "LIST_TAG" ${args_LIST_TAG})
-    endif()
 
     if(NOT "${args_TARGET_NAME}" STREQUAL "")
         list(APPEND call_args "TARGET_NAME" ${args_TARGET_NAME})
@@ -84,18 +84,26 @@ function(get_test_source_dir_condition path output)
         list(APPEND call_args "SRC_EXT" ${args_SRC_EXT})
     endif()
 
-    if(NOT "${args_SRC_DIR}" STREQUAL "")
-        list(APPEND call_args "SRC_DIR" ${args_SRC_DIR})
+    if(NOT "${args_SRC_CURR_DIR}" STREQUAL "")
+        list(APPEND call_args "SRC_CURR_DIR" ${args_SRC_CURR_DIR})
     endif()
 
-    if(NOT "${args_SRC_RELATIVE_DIR}" STREQUAL "")
-        list(APPEND call_args "SRC_RELATIVE_DIR" ${args_SRC_RELATIVE_DIR})
+    if(NOT "${args_SRC_RELATIVE_CURR_DIR}" STREQUAL "")
+        list(APPEND call_args "SRC_RELATIVE_CURR_DIR" ${args_SRC_RELATIVE_CURR_DIR})
+    endif()
+
+    if (NOT "${args_SRC_CURR_DIRNAME}" STREQUAL "")
+        list(APPEND call_args "SRC_CURR_DIRNAME" ${args_SRC_CURR_DIRNAME})
     endif()
     
-    add_test_executable_dir_header_condition(${path} result ${case_sensitive_arg}
+    add_test_executable_dir_header_condition(result ${case_sensitive_arg}
         BASE_DIR ${get_test_source_dir_condition_BASE_DIR}
+        PATH ${get_test_source_dir_condition_PATH}
         RELATIVE_PATH ${get_test_source_dir_condition_RELATIVE_PATH}
         FILENAME ${get_test_source_dir_condition_FILENAME}
+        CURR_DIR ${get_test_source_dir_condition_CURR_DIR}
+        RELATIVE_CURR_DIR ${get_test_source_dir_condition_RELATIVE_CURR_DIR}
+        CURR_DIRNAME ${get_test_source_dir_condition_CURR_DIRNAME}
         INCLUDE_DIR ${get_test_source_dir_condition_INCLUDE_DIR}
         ${call_args} ARGS ${args_HEADER_CONDITION_ARGS})
 

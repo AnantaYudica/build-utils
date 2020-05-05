a41bc11406a03a3a6906ccf392f74a508122324d
Ananta Yudica <nta040689@gmail.com>
2020-05-05 17:53:36 +0700

    Removed argument name 'path' Added argument flag PATH, CURR_DIR, RELATIVE_CURR_DIR and CURR_DIRNAME Added argument option DIRECTORY Removed argument list flag 'LIST_EXT' in function add_test_executable_dir_src_filter Added argument list flag 'LIST_ExT' in ARGS Added check condition is directory

--- include/add_test_executable_dir/src_filter/list_ext.cmake
+++ include/add_test_executable_dir/src_filter/list_ext.cmake
@@ -1,13 +1,18 @@
-function(add_test_executable_dir_src_filter path cond)
+function(add_test_executable_dir_src_filter cond)
     
-    cmake_parse_arguments(add_test_executable_dir_src_filter "CASE_SENSITIVE" 
-        "LEVEL;BASE_DIR;RELATIVE_PATH;FILENAME;NAME;TAG:EXT;INCLUDE_DIR" 
-        "LIST_EXT;ARGS" ${ARGN}) 
+    string(CONCAT one_options "LEVEL;BASE_DIR;PATH;RELATIVE_PATH;FILENAME;DIRNAME"
+        ";CURR_DIR;RELATIVE_CURR_DIR;CURR_DIRNAME;INCLUDE_DIR;NAME;TAG:EXT")
+    cmake_parse_arguments(add_test_executable_dir_src_filter 
+        "CASE_SENSITIVE;DIRECTORY" "${one_options}" "ARGS" ${ARGN}) 
 
+    cmake_parse_arguments(args "" "" "LIST_EXT" 
+        ${add_test_executable_dir_src_filter_ARGS}) 
+
+    set(is_directory ${add_test_executable_dir_src_filter_DIRECTORY})
     set(is_case_sensitive ${add_test_executable_dir_src_filter_CASE_SENSITIVE})
-    set(list_ext ${add_test_executable_dir_src_filter_LIST_EXT})
+    set(list_ext ${args_LIST_EXT})
 
-    if (NOT ${list_ext} STREQUAL "")
+    if (NOT ${list_ext} STREQUAL "" AND (NOT is_directory))
         set(result FALSE)
 
         if(is_case_sensitive)

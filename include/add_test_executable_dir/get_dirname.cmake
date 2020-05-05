e6ef551fd353e13c637edcc3de35567bcdd2bd18
Ananta Yudica <nta040689@gmail.com>
2020-05-06 00:54:50 +0700

    Renamed function from add_test_executable_dir_get_dir_name to add_test_executable_dir_get_dirname

--- include/add_test_executable_dir/get_dirname.cmake
+++ include/add_test_executable_dir/get_dirname.cmake
@@ -1,9 +1,9 @@
-function(add_test_executable_dir_get_dir_name output)
+function(add_test_executable_dir_get_dirname output)
     
-    cmake_parse_arguments(add_test_executable_dir_get_dir_name "" 
+    cmake_parse_arguments(add_test_executable_dir_get_dirname "" 
         "BASE_DIR;PATH;RELATIVE_PATH;DIRNAME;NAME;TAG;INCLUDE_DIR" 
         "ARGS" ${ARGN}) 
 
-    set(${output} "${add_test_executable_dir_get_dir_name_DIRNAME}" PARENT_SCOPE)
+    set(${output} "${add_test_executable_dir_get_dirname_DIRNAME}" PARENT_SCOPE)
 
-endfunction(add_test_executable_dir_get_dir_name)
+endfunction(add_test_executable_dir_get_dirname)

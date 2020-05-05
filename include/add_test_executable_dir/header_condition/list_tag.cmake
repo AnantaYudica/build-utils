fd6184f189d29264ae4aadbb03a39ad03a9eddb3
Ananta Yudica <nta040689@gmail.com>
2020-05-06 03:50:55 +0700

    Added argument option flag PATH, CURR_DIR, RELATIVE_CURR_DIR, CURR_DIRNAME, GET_NAME, GET_TAG, TAG_CONDITION, TAG_DELIMITER, TARGET_NAME, TARGET_DIR, SRC_PATH, SRC_BASE_DIR, SRC_RELATIVE_PATH, SRC_FILENAME, SRC_NAME, SRC_TAG, SRC_EXT, SRC_CURR_DIR, SRC_RELATIVE_CURR_DIR and SRC_CURR_DIRNAME Removed argument option flag GET_NAME, GET_TAG, TAG_CONDITION and TAG_DELIMITER in ARGS

--- include/add_test_executable_dir/header_condition/list_tag.cmake
+++ include/add_test_executable_dir/header_condition/list_tag.cmake
@@ -1,6 +1,15 @@
-function(add_test_executable_dir_header_condition path output)
+function(add_test_executable_dir_header_condition output)
+    string(CONCAT one_options "BASE_DIR;PATH;RELATIVE_PATH;FILENAME"
+        ";CURR_DIR;RELATIVE_CURR_DIR;CURR_DIRNAME;INCLUDE_DIR"
+        ";GET_NAME;GET_TAG;TAG_CONDITION;TAG_DELIMITER"
+        ";TARGET_NAME;TARGET_DIR;SRC_PATH;SRC_BASE_DIR;SRC_RELATIVE_PATH"
+        ";SRC_FILENAME;SRC_NAME;SRC_TAG;SRC_EXT;SRC_CURR_DIR"
+        ";SRC_RELATIVE_CURR_DIR;SRC_CURR_DIRNAME") 
     cmake_parse_arguments(add_test_executable_dir_header_condition "CASE_SENSITIVE" 
-        "BASE_DIR;RELATIVE_PATH;FILENAME;INCLUDE_DIR" "ARGS" ${ARGN}) 
+        "${one_options}" "ARGS" ${ARGN}) 
+
+    cmake_parse_arguments(args "" "" 
+        "LIST_TAG" ${add_test_executable_dir_header_condition_ARGS})
 
     set(filename "${add_test_executable_dir_header_condition_FILENAME}")
 
@@ -8,10 +17,7 @@ function(add_test_executable_dir_header_condition path output)
     if (NOT "${get_test_source_dir_filter_CASE_SENSITIVE}" STREQUAL "")
         set(is_case_sensitive ${get_test_source_dir_filter_CASE_SENSITIVE})
     endif()
-
-    cmake_parse_arguments(args "" "GET_NAME;GET_TAG;TAG_CONDITION;TAG_DELIMITER" 
-        "LIST_TAG" ${add_test_executable_dir_header_condition_ARGS})
-        
+  
     set(list_tag ${args_LIST_TAG})
 
     if (NOT "${list_tag}" STREQUAL "")

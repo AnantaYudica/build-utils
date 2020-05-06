4cbc44d55e61a215613eb1ac6ed5e97ae1b28d5d
Ananta Yudica <nta040689@gmail.com>
2020-05-06 09:49:06 +0700

    Removed argument flag dir Renamed argument flag from RELATIVE_DIR and DIR_NAME to RELATIVE_PATH and DIRNAME Added argument flag PATH, CURR_DIR, RELATIVE_CURR_DIR and CURR_DIRNAME

--- include/add_test_executable_dir/get_target_dir.cmake
+++ include/add_test_executable_dir/get_target_dir.cmake
@@ -1,9 +1,10 @@
-function(add_test_executable_dir_get_target_dir dir out_dir_name)
+function(add_test_executable_dir_get_target_dir out_dir_name)
     
+    string(CONCAT one_options "BASE_DIR;PATH;RELATIVE_PATH;DIRNAME;NAME;TAG"
+        ";CURR_DIR;RELATIVE_CURR_DIR;CURR_DIRNAME;INCLUDE_DIR")
     cmake_parse_arguments(add_test_executable_dir_get_target_dir "" 
-        "BASE_DIR;RELATIVE_DIR;DIR_NAME;NAME;TAG;INCLUDE_DIR" 
-        "ARGS" ${ARGN}) 
+        "${one_options}" "ARGS" ${ARGN}) 
 
-    set(${out_dir_name} "${add_test_executable_dir_get_target_dir_DIR_NAME}")
+    set(${out_dir_name} "${add_test_executable_dir_get_target_dir_DIRNAME}")
 
 endfunction(add_test_executable_dir_get_target_dir)

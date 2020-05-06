fbebaf6c64a541e6bcdaeb03db92aa1d0068bb6d
Ananta Yudica <nta040689@gmail.com>
2020-05-06 07:52:18 +0700

    Removed argument name path Added argument flag PATH and CURR_DIRNAME Renamed argument flag from DIR and RELATIVE_DIR to CURR_DIR and RELATIVE_CURR_DIR

--- include/add_test_executable_dir/get_target_compile.cmake
+++ include/add_test_executable_dir/get_target_compile.cmake
@@ -1,16 +1,15 @@
-function(add_test_executable_dir_get_target_compile path out_list_def
-    out_list_option)
+function(add_test_executable_dir_get_target_compile out_list_def out_list_option)
     
     string(CONCAT one_options "TARGET_NAME;TARGET_DIR"
-        ";BASE_DIR;RELATIVE_PATH;FILENAME;NAME;TAG;EXT"
-        ";DIR;RELATIVE_DIR;INCLUDE_DIR")
+        ";BASE_DIR;PATH;RELATIVE_PATH;FILENAME;NAME;TAG;EXT"
+        ";CURR_DIR;RELATIVE_CURR_DIR;CURR_DIRNAME;INCLUDE_DIR")
     cmake_parse_arguments(add_test_executable_dir_get_target_compile "" 
-        "${one_options}"
-        "DEFAULT_LIST_DEF;DEFAULT_LIST_OPTION;ARGS" ${ARGN}) 
+        "${one_options}" "DEFAULT_LIST_DEF;DEFAULT_LIST_OPTION;ARGS" ${ARGN}) 
 
-    set(${out_list_def} 
-        "${add_test_executable_dir_get_target_compile_DEFAULT_LIST_DEF}" PARENT_SCOPE)
-    set(${out_list_option} 
-        "${add_test_executable_dir_get_target_compile_DEFAULT_LIST_OPTION}" PARENT_SCOPE)
+    set(default_list_def ${add_test_executable_dir_get_target_compile_DEFAULT_LIST_DEF})
+    set(default_list_option ${add_test_executable_dir_get_target_compile_DEFAULT_LIST_OPTION})
+
+    set(${out_list_def} "${default_list_def}" PARENT_SCOPE)
+    set(${out_list_option} "${default_list_option}" PARENT_SCOPE)
     
 endfunction(add_test_executable_dir_get_target_compile)

7bf61f8eb2148dc858b64a52df12d280d728e504
Ananta Yudica <nta040689@gmail.com>
2020-05-07 06:40:22 +0700

    Added argument flag PREFIX and PREFIX_DIR Added variable PREFIX Updated value of out_target_name

--- include/add_test_executable_dir/get_target_name.cmake
+++ include/add_test_executable_dir/get_target_name.cmake
@@ -1,12 +1,14 @@
 function(add_test_executable_dir_get_target_name out_target_name)
     
-    string(CONCAT one_options "BASE_DIR;PATH;RELATIVE_PATH;FILENAME;NAME;TAG;EXT"
+    string(CONCAT one_options "PREFIX;PREFIX_DIR"
+        ";BASE_DIR;PATH;RELATIVE_PATH;FILENAME;NAME;TAG;EXT"
         ";CURR_DIR;RELATIVE_CURR_DIR;CURR_DIRNAME;INCLUDE_DIR")
     cmake_parse_arguments(add_test_executable_dir_get_target_name "" 
         "${one_options}" "ARGS" ${ARGN}) 
 
+    set(prefix ${add_test_executable_dir_get_target_name_PREFIX})
     set(name ${add_test_executable_dir_get_target_name_NAME})
 
-    set(${out_target_name} "${name}" PARENT_SCOPE)
+    set(${out_target_name} "${prefix}${name}" PARENT_SCOPE)
 
 endfunction(add_test_executable_dir_get_target_name)

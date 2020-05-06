662df2eeaa83a0d223ab1d5158d7d1b4f843128d
Ananta Yudica <nta040689@gmail.com>
2020-05-06 21:59:43 +0700

    Removed argument name path Added argument flag PATH and CURR_DIRNAME Renamed argument flag from DIR and RELATIVE_DIR to CURR_DIR and RELATIVE_CURR_DIR

--- include/add_test_executable_dir/get_target_other_src.cmake
+++ include/add_test_executable_dir/get_target_other_src.cmake
@@ -1,12 +1,13 @@
-function(add_test_executable_dir_get_target_other_src path out_list_src)
+function(add_test_executable_dir_get_target_other_src out_list_src)
     
     string(CONCAT one_options "TARGET_NAME;TARGET_DIR"
-        ";BASE_DIR;RELATIVE_PATH;FILENAME;NAME;TAG;EXT"
-        ";DIR;RELATIVE_DIR;INCLUDE_DIR")
+        ";BASE_DIR;PATH;RELATIVE_PATH;FILENAME;NAME;TAG;EXT"
+        ";CURR_DIR;RELATIVE_CURR_DIR;CURR_DIRNAME;INCLUDE_DIR")
     cmake_parse_arguments(add_test_executable_dir_get_target_other_src "" 
-        "${one_options}" 
-        "ARGS;DEFAULT_LIST_SRC" ${ARGN}) 
+        "${one_options}" "ARGS;DEFAULT_LIST_SRC" ${ARGN}) 
 
-    set(${out_list_src} "${add_test_executable_dir_get_target_other_src_DEFAULT_LIST_SRC}" PARENT_SCOPE)
+    set(default_list_src ${add_test_executable_dir_get_target_other_src_DEFAULT_LIST_SRC})
+
+    set(${out_list_src} "${default_list_src}" PARENT_SCOPE)
     
 endfunction(add_test_executable_dir_get_target_other_src)

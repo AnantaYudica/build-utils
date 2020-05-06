1a1119fbc450247e8cfd964ca336726328afc20e
Ananta Yudica <nta040689@gmail.com>
2020-05-06 18:33:31 +0700

    Removed argument name path Added argument flag PATH and CURR_DIRNAME Renamed argument flag from DIR and RELATIVE_DIR to CURR_DIR and RELATIVE_CURR_DIR

--- include/add_test_executable_dir/get_target_link.cmake
+++ include/add_test_executable_dir/get_target_link.cmake
@@ -1,12 +1,13 @@
-function(add_test_executable_dir_get_target_link path out_list_lib)
+function(add_test_executable_dir_get_target_link out_list_lib)
     
     string(CONCAT one_options "TARGET_NAME;TARGET_DIR;"
-        ";BASE_DIR;RELATIVE_PATH;FILENAME;NAME;TAG;EXT"
-        ";DIR;RELATIVE_DIR;INCLUDE_DIR")
+        ";BASE_DIR;PATH;RELATIVE_PATH;FILENAME;NAME;TAG;EXT"
+        ";CURR_DIR;RELATIVE_CURR_DIR;CURR_DIRNAME;INCLUDE_DIR")
     cmake_parse_arguments(add_test_executable_dir_get_target_link "" 
-        "${one_options}" 
-        "ARGS;DEFAULT_LIST_LIB" ${ARGN}) 
+        "${one_options}" "ARGS;DEFAULT_LIST_LIB" ${ARGN}) 
 
-    set(${out_list_lib} "${add_test_executable_dir_get_target_link_DEFAULT_LIST_LIB}" PARENT_SCOPE)
+    set(default_list_lib ${add_test_executable_dir_get_target_link_DEFAULT_LIST_LIB})
+
+    set(${out_list_lib} "${default_list_lib}" PARENT_SCOPE)
     
 endfunction(add_test_executable_dir_get_target_link)

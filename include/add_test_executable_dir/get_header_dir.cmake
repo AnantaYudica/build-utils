a1072356c47b5311ac77e9c09fa946f268030d1f
Ananta Yudica <nta040689@gmail.com>
2020-05-06 07:41:03 +0700

    Removed argument name path Added argument flag PATH and CURR_DIRNAME Renamed argument flag from DIR and RELATIVE_DIR to CURR_DIR and RELATIVE_CURR_DIR

--- include/add_test_executable_dir/get_header_dir.cmake
+++ include/add_test_executable_dir/get_header_dir.cmake
@@ -1,16 +1,16 @@
-function(add_test_executable_dir_get_header_dir path header_dir)
+function(add_test_executable_dir_get_header_dir header_dir)
     string(CONCAT one_options "TARGET_NAME;TARGET_DIR;BASE_HEADER_DIR"
-        ";BASE_DIR;RELATIVE_PATH;FILENAME;NAME;TAG;EXT"
-        ";DIR;RELATIVE_DIR;INCLUDE_DIR")
+        ";BASE_DIR;PATH;RELATIVE_PATH;FILENAME;NAME;TAG;EXT"
+        ";CURR_DIR;RELATIVE_CURR_DIR;CURR_DIRNAME;INCLUDE_DIR")
     string(CONCAT list_options "ARGS")
     cmake_parse_arguments(add_test_executable_dir_get_header_dir "" 
         "${one_options}" "${list_options}" ${ARGN}) 
 
-    set(dir ${add_test_executable_dir_get_header_dir_DIR})
+    set(dir ${add_test_executable_dir_get_header_dir_CURR_DIR})
     set(base_header_dir ${add_test_executable_dir_get_header_dir_BASE_HEADER_DIR})
 
     if (NOT "${base_header_dir}" STREQUAL "")
-        set(relative_dir ${add_test_executable_dir_get_header_dir_RELATIVE_DIR})
+        set(relative_dir ${add_test_executable_dir_get_header_dir_RELATIVE_CURR_DIR})
         get_filename_component(current_header_dir "${base_header_dir}/${relative_dir}" ABSOLUTE)
         set(${header_dir} ${current_header_dir} PARENT_SCOPE)
     else()

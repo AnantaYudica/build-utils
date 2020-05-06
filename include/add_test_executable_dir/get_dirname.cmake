033b558a028d9b1ebc1c6a82596065375d52f025
Ananta Yudica <nta040689@gmail.com>
2020-05-06 07:27:48 +0700

    Added argument flag CURR_DIR, RELATIVE_CURR_DIR and CURR_DIRNAME

--- include/add_test_executable_dir/get_dirname.cmake
+++ include/add_test_executable_dir/get_dirname.cmake
@@ -1,8 +1,8 @@
 function(add_test_executable_dir_get_dirname output)
-    
+    string(CONCAT one_options "BASE_DIR;PATH;RELATIVE_PATH;DIRNAME;NAME;TAG"
+        "CURR_DIR;RELATIVE_CURR_DIR;CURR_DIRNAME;INCLUDE_DIR")
     cmake_parse_arguments(add_test_executable_dir_get_dirname "" 
-        "BASE_DIR;PATH;RELATIVE_PATH;DIRNAME;NAME;TAG;INCLUDE_DIR" 
-        "ARGS" ${ARGN}) 
+        "${one_options}" "ARGS" ${ARGN}) 
 
     set(${output} "${add_test_executable_dir_get_dirname_DIRNAME}" PARENT_SCOPE)
 

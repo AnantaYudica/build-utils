c48ef9ea227d95054c58b82531835b118959c2db
Ananta Yudica <nta040689@gmail.com>
2020-05-06 22:19:50 +0700

    Removed argument name path Added argument flag PATH and CURR_DIRNAME Renamed argument flag from DIR and RELATIVE_DIR to CURR_DIR and RELATIVE_CURR_DIR

--- include/add_test_executable_dir/get_test_command.cmake
+++ include/add_test_executable_dir/get_test_command.cmake
@@ -1,12 +1,13 @@
-function(add_test_executable_dir_get_test_command path out_list_arg)
+function(add_test_executable_dir_get_test_command out_list_arg)
     
     string(CONCAT one_options "TARGET_NAME;TARGET_DIR"
-        ";BASE_DIR;RELATIVE_PATH;FILENAME;NAME;TAG;EXT"
-        ";DIR;RELATIVE_DIR;INCLUDE_DIR")
+        ";BASE_DIR;PATH;RELATIVE_PATH;FILENAME;NAME;TAG;EXT"
+        ";CURR_DIR;RELATIVE_CURR_DIR;CURR_DIRNAME;INCLUDE_DIR")
     cmake_parse_arguments(add_test_executable_dir_get_test_command "" 
-        "${one_options}" 
-        "ARGS;DEFAULT_LIST_ARG" ${ARGN}) 
+        "${one_options}" "ARGS;DEFAULT_LIST_ARG" ${ARGN}) 
 
-    set(${out_list_arg} "${add_test_executable_dir_get_test_command_DEFAULT_LIST_ARG}" PARENT_SCOPE)
+    set(default_list_arg ${add_test_executable_dir_get_test_command_DEFAULT_LIST_ARG})
+    
+    set(${out_list_arg} "${default_list_arg}" PARENT_SCOPE)
     
 endfunction(add_test_executable_dir_get_test_command)

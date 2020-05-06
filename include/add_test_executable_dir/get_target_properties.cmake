5c279ee593ac79324cfd36e2a7b15faccc992e6b
Ananta Yudica <nta040689@gmail.com>
2020-05-06 22:01:46 +0700

    Removed argument name path Added argument flag PATH and CURR_DIRNAME Renamed argument flag from DIR and RELATIVE_DIR to CURR_DIR and RELATIVE_CURR_DIR

--- include/add_test_executable_dir/get_target_properties.cmake
+++ include/add_test_executable_dir/get_target_properties.cmake
@@ -1,12 +1,13 @@
-function(add_test_executable_dir_get_target_properties path out_list_property)
+function(add_test_executable_dir_get_target_properties out_list_property)
     
     string(CONCAT one_options "TARGET_NAME;TARGET_DIR"
-        ";BASE_DIR;RELATIVE_PATH;FILENAME;NAME;TAG;EXT"
-        ";DIR;RELATIVE_DIR;INCLUDE_DIR")
+        ";BASE_DIR;PATH;RELATIVE_PATH;FILENAME;NAME;TAG;EXT"
+        ";CURR_DIR;RELATIVE_CURR_DIR;CURR_DIRNAME;INCLUDE_DIR")
     cmake_parse_arguments(add_test_executable_dir_get_target_properties "" 
-        "${one_options}" 
-        "ARGS;DEFAULT_LIST_PROPERTY" ${ARGN}) 
+        "${one_options}" "ARGS;DEFAULT_LIST_PROPERTY" ${ARGN}) 
 
-    set(${out_list_property} "${add_test_executable_dir_get_target_properties_DEFAULT_LIST_PROPERTY}" PARENT_SCOPE)
+    set(default_list_property ${add_test_executable_dir_get_target_properties_DEFAULT_LIST_PROPERTY})
+
+    set(${out_list_property} "${default_list_property}" PARENT_SCOPE)
     
 endfunction(add_test_executable_dir_get_target_properties)

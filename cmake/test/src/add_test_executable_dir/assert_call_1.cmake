ff8b754443f46f26acf15e54faaa89bec5c62341
Ananta Yudica <nta040689@gmail.com>
2020-05-08 05:28:56 +0700

    Added functions assert_call for output {LIST_TARGET_NAME, LIST_TARGET_DIR, LIST_TARGET_LIB, LIST_TARGET_INCLUDE_DIR, LIST_TARGET_COMPILE_DEF, LIST_TARGET_COMPILE_OPTION, LIST_TARGET_PROPERTY, LIST_TARGET_CMD_ARG, LIST_SRC, LIST_HEADER and LIST_GROUP_NAME} from function add_test_executable_dir

---
+++ cmake/test/src/add_test_executable_dir/assert_call_1.cmake
@@ -0,0 +1,14 @@
+function(assert_call)
+    cmake_parse_arguments(assert_call "" "OUTPUT" "ARGS" ${ARGN}) 
+
+    parse_args(dir result_call_args ARGS ${assert_call_ARGS})
+
+    add_test_executable_dir(${dir} ${result_call_args} LIST_TARGET_NAME output)
+
+    if (NOT "${output}" STREQUAL "")
+        set(${assert_call_OUTPUT} "${output}" PARENT_SCOPE)
+    else()
+        set(${assert_call_OUTPUT} "EMPTY" PARENT_SCOPE)
+    endif()
+
+endfunction(assert_call)

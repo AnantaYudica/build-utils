70c63b1d07401ad8d2c185d2e3ad176459763d66
Ananta Yudica <nta040689@gmail.com>
2020-05-12 12:29:27 +0700

    Fixed list string conjunction

--- include/add_test_executable_dir/src_filter/default.cmake
+++ include/add_test_executable_dir/src_filter/default.cmake
@@ -1,7 +1,7 @@
 function(add_test_executable_dir_src_filter cond)
     
     string(CONCAT one_options "LEVEL;BASE_DIR;PATH;RELATIVE_PATH;FILENAME;DIRNAME"
-        ";CURR_DIR;RELATIVE_CURR_DIR;CURR_DIRNAME;INCLUDE_DIR;NAME;TAG:EXT")
+        ";CURR_DIR;RELATIVE_CURR_DIR;CURR_DIRNAME;INCLUDE_DIR;NAME;TAG;EXT")
     cmake_parse_arguments(add_test_executable_dir_src_filter 
         "CASE_SENSITIVE;DIRECTORY" "${one_options}" "ARGS" ${ARGN}) 
     

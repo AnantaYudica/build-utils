e884b2d83979f2b36e0c8584584255d1b3a43f06
Ananta Yudica <nta040689@gmail.com>
2020-05-13 17:11:55 +0700

    Added implementation of function add_test_executable_dir_get_dirname

---
+++ cmake/test/src/add_test_executable_dir/impl/get_dirname.cmake
@@ -0,0 +1,18 @@
+function(add_test_executable_dir_get_dirname output)
+    string(CONCAT one_options "BASE_DIR;PATH;RELATIVE_PATH;DIRNAME;NAME;TAG"
+        "CURR_DIR;RELATIVE_CURR_DIR;CURR_DIRNAME;INCLUDE_DIR")
+    cmake_parse_arguments(add_test_executable_dir_get_dirname "" 
+        "${one_options}" "ARGS" ${ARGN}) 
+assert_debug("CALLER")
+    cmake_parse_arguments(args "UPPER" "" "" 
+        ${add_test_executable_dir_get_dirname_ARGS})
+
+    if (args_UPPER)
+        string(TOUPPER "${add_test_executable_dir_get_dirname_DIRNAME}" dirname)
+    else()
+        set(dirname add_test_executable_dir_get_dirname_DIRNAME)
+    endif()
+
+    set(${output} "${dirname}" PARENT_SCOPE)
+
+endfunction(add_test_executable_dir_get_dirname)

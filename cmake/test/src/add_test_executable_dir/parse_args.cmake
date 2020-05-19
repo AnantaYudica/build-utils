31c97bb9b3d5941bcf7303a3f452bc810717b53b
Ananta Yudica <nta040689@gmail.com>
2020-05-19 11:31:00 +0700

    Updated name from *compile_option to *compile_option_arg

--- cmake/test/src/add_test_executable_dir/parse_args.cmake
+++ cmake/test/src/add_test_executable_dir/parse_args.cmake
@@ -42,7 +42,7 @@ function(parse_args output_dir output_call_args)
         ";header_group_name;src_group_name;other_src_group_name")
     
     string(CONCAT list_options "${list_options}"
-        ";list_compile_def_arg;list_compile_option;list_include_dir;list_lib"
+        ";list_compile_def_arg;list_compile_option_arg;list_include_dir;list_lib"
         ";list_other_src;list_property;list_test_cmd_arg")
 
     string(CONCAT one_options "${one_options}" 
@@ -311,8 +311,8 @@ function(parse_args output_dir output_call_args)
         list(APPEND call_args "LIST_COMPILE_DEF_ARG" ${args_list_compile_def_arg})
     endif()
     
-    if (NOT "${args_list_compile_option}" STREQUAL "")
-        list(APPEND call_args "LIST_COMPILE_OPTION" ${args_list_compile_option})
+    if (NOT "${args_list_compile_option_arg}" STREQUAL "")
+        list(APPEND call_args "LIST_COMPILE_OPTION_ARG" ${args_list_compile_option_arg})
     endif()
     
     if (NOT "${args_list_include_dir}" STREQUAL "")
--- include/add_test_executable_dir.cmake
+++ include/add_test_executable_dir.cmake
@@ -34,7 +34,7 @@ function(add_test_executable_dir_recv base_dir dir
         ";INCLUDE_DIR")
 
     string(CONCAT list_options "${list_options}"
-        ";LIST_COMPILE_DEF_ARG;LIST_COMPILE_OPTION;LIST_INCLUDE_DIR;LIST_LIB"
+        ";LIST_COMPILE_DEF_ARG;LIST_COMPILE_OPTION_ARG;LIST_INCLUDE_DIR;LIST_LIB"
         ";LIST_OTHER_SRC;LIST_PROPERTY;LIST_TEST_CMD_ARG")
 
     cmake_parse_arguments(add_test_executable_dir_recv "" 
@@ -77,7 +77,7 @@ function(add_test_executable_dir_recv base_dir dir
     set(header_group_name ${add_test_executable_dir_recv_HEADER_GROUP_NAME})
     set(other_src_group_name ${add_test_executable_dir_recv_OTHER_SRC_GROUP_NAME})
     set(list_compile_def_arg ${add_test_executable_dir_recv_LIST_COMPILE_DEF_ARG})
-    set(list_compile_option ${add_test_executable_dir_recv_LIST_COMPILE_OPTION})
+    set(list_compile_option_arg ${add_test_executable_dir_recv_LIST_COMPILE_OPTION_ARG})
     set(list_include_dir ${add_test_executable_dir_recv_LIST_INCLUDE_DIR})
     set(list_lib ${add_test_executable_dir_recv_LIST_LIB})
     set(list_other_src ${add_test_executable_dir_recv_LIST_OTHER_SRC})
@@ -221,7 +221,7 @@ function(add_test_executable_dir_recv base_dir dir
                     HEADER_GROUP_NAME ${header_group_name}
                     OTHER_SRC_GROUP_NAME ${other_src_group_name}
                     LIST_COMPILE_DEF_ARG ${list_compile_def_arg}
-                    LIST_COMPILE_OPTION ${list_compile_option}
+                    LIST_COMPILE_OPTION_ARG ${list_compile_option_arg}
                     LIST_INCLUDE_DIR ${list_include_dir}
                     LIST_LIB ${list_lib}
                     LIST_OTHER_SRC ${list_other_src}
@@ -347,7 +347,7 @@ function(add_test_executable_dir_recv base_dir dir
                     add_test_executable_dir_get_target_compile(
                         target_list_compile_def_arg target_list_compile_option_arg
                         DEFAULT_LIST_DEF_ARG ${list_compile_def_arg}
-                        DEFAULT_LIST_OPTION ${list_compile_option}
+                        DEFAULT_LIST_OPTION_ARG ${list_compile_option_arg}
                         TARGET_NAME ${target_name} TARGET_DIR ${prefix_dir}
                         BASE_DIR ${base_dir} PATH ${it} RELATIVE_PATH ${relative_path} 
                         FILENAME ${filename} NAME ${src_name} TAG ${src_tag} EXT ${src_ext} 
@@ -664,7 +664,7 @@ function(add_test_executable_dir dir)
         ";HEADER_GROUP_NAME;SRC_GROUP_NAME;OTHER_SRC_GROUP_NAME")
     
     string(CONCAT list_options "${list_options}"
-        ";LIST_COMPILE_DEF_ARG;LIST_COMPILE_OPTION;LIST_INCLUDE_DIR;LIST_LIB"
+        ";LIST_COMPILE_DEF_ARG;LIST_COMPILE_OPTION_ARG;LIST_INCLUDE_DIR;LIST_LIB"
         ";LIST_OTHER_SRC;LIST_PROPERTY;LIST_TEST_CMD_ARG")
 
     string(CONCAT one_options "${one_options}" 
@@ -1141,7 +1141,7 @@ function(add_test_executable_dir dir)
         HEADER_GROUP_NAME ${header_group_name}
         OTHER_SRC_GROUP_NAME ${other_src_group_name}
         LIST_COMPILE_DEF_ARG ${add_test_executable_dir_LIST_COMPILE_DEF_ARG}
-        LIST_COMPILE_OPTION ${add_test_executable_dir_LIST_COMPILE_OPTION}
+        LIST_COMPILE_OPTION_ARG ${add_test_executable_dir_LIST_COMPILE_OPTION_ARG}
         LIST_INCLUDE_DIR ${add_test_executable_dir_LIST_INCLUDE_DIR}
         LIST_LIB ${add_test_executable_dir_LIST_LIB}
         LIST_OTHER_SRC ${add_test_executable_dir_LIST_OTHER_SRC}

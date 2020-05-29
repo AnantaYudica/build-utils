43af50f1fc5d39db21d3fcf5d92f929434621cac
Ananta Yudica <nta040689@gmail.com>
2020-05-29 08:16:30 +0700

    Updated name from BUILD_UTILS_BASE_SOURCE_DIR to BUILD_UTILS_CMAKE_BASE_SRC_DIR

--- cmake/set_variables.cmake
+++ cmake/set_variables.cmake
@@ -1,30 +1,19 @@
 
 function(set_variables)
     cmake_parse_arguments(set_variables "" 
-        "BASE_SOURCE_DIR;INCLUDE_DIR;TEST_BASE_DIR;TEST_UTILS_DIR;TEST_SOURCE_DIR" "" ${ARGN}) 
-
-    set(l_base_source_dir "")
-
-    if("${EMPTY}${set_variables_BASE_SOURCE_DIR}" STREQUAL "${EMPTY}")
-        get_filename_component(l_base_source_dir "${CMAKE_SOURCE_DIR}" ABSOLUTE)
-    else()    
-        get_filename_component(l_base_source_dir "${set_variables_BASE_SOURCE_DIR}" ABSOLUTE)
-    endif()
-
-    if (EXISTS "${l_base_source_dir}")
-        set(BUILD_UTILS_BASE_SOURCE_DIR ${l_base_source_dir} 
-            CACHE PATH "build utils base directory" FORCE)
-    elseif(NOT EXISTS "${l_base_source_dir}")
-        set(BUILD_UTILS_BASE_SOURCE_DIR ${CMAKE_SOURCE_DIR} 
-            CACHE PATH "build utils base directory" FORCE)
-    else()
-        message(FATAL_ERROR "variable \"BASE_SOURCE_DIR\"" )
+        "INCLUDE_DIR;TEST_BASE_DIR;TEST_UTILS_DIR;TEST_SOURCE_DIR" "" ${ARGN}) 
+    
+    if (NOT DEFINED BUILD_UTILS_CMAKE_BASE_SRC_DIR)
+        
+        get_filename_component(l_cmake_base_src_dir "${CMAKE_SOURCE_DIR}" ABSOLUTE)
+        set(BUILD_UTILS_CMAKE_BASE_SRC_DIR ${l_cmake_base_src_dir} 
+            CACHE PATH "build utils cmake base source directory" FORCE)
     endif()
     
     set(l_include_dir "")
     
     if("${EMPTY}${set_variables_INCLUDE_DIR}" STREQUAL "${EMPTY}")
-        get_filename_component(l_include_dir "${BUILD_UTILS_BASE_SOURCE_DIR}/../include" ABSOLUTE)
+        get_filename_component(l_include_dir "${BUILD_UTILS_CMAKE_BASE_SRC_DIR}/../include" ABSOLUTE)
     else()
         get_filename_component(l_include_dir "${set_variables_INCLUDE_DIR}" ABSOLUTE)
     endif()
@@ -33,7 +22,7 @@ function(set_variables)
         set(BUILD_UTILS_INCLUDE_DIR ${l_include_dir} 
             CACHE PATH "build utils include directory" FORCE)
     elseif(NOT EXISTS "${l_include_dir}")
-        get_filename_component(l_include_dir "${BUILD_UTILS_BASE_SOURCE_DIR}/../include" ABSOLUTE)
+        get_filename_component(l_include_dir "${BUILD_UTILS_CMAKE_BASE_SRC_DIR}/../include" ABSOLUTE)
         set(BUILD_UTILS_INCLUDE_DIR "${l_include_dir}" 
             CACHE PATH "build utils include directory" FORCE)
     else()
@@ -43,7 +32,7 @@ function(set_variables)
     set(l_test_base_dir "")
 
     if ("${EMPTY}${set_variables_TEST_BASE_DIR}" STREQUAL "${EMPTY}")
-        get_filename_component(l_test_base_dir "${BUILD_UTILS_BASE_SOURCE_DIR}/test" ABSOLUTE)
+        get_filename_component(l_test_base_dir "${BUILD_UTILS_CMAKE_BASE_SRC_DIR}/test" ABSOLUTE)
     else()
         get_filename_component(l_test_base_dir "${set_variables_TEST_BASE_DIR}" ABSOLUTE)
     endif()
@@ -51,7 +40,7 @@ function(set_variables)
     if (EXISTS "${l_test_base_dir}")
         set(BUILD_UTILS_TEST_BASE_DIR ${l_test_base_dir} CACHE PATH "test directory" FORCE)
     elseif(NOT EXISTS "${l_test_base_dir}")
-        get_filename_component(l_test_base_dir "${BUILD_UTILS_BASE_SOURCE_DIR}/test" ABSOLUTE)
+        get_filename_component(l_test_base_dir "${BUILD_UTILS_CMAKE_BASE_SRC_DIR}/test" ABSOLUTE)
         set(BUILD_UTILS_TEST_BASE_DIR "${l_test_base_dir}" CACHE PATH "test directory" FORCE)
     else()
         message(FATAL_ERROR "variable \"TEST_BASE_DIR\"" )
@@ -91,6 +80,6 @@ function(set_variables)
         message(FATAL_ERROR "variable \"TEST_SOURCE_DIR\"" )
     endif()
 
-    include(${BUILD_UTILS_BASE_SOURCE_DIR}/include_build_util.cmake)
+    include(${BUILD_UTILS_CMAKE_BASE_SRC_DIR}/include_build_util.cmake)
 
 endfunction(set_variables)
